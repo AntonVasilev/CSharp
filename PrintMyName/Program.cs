ed0bc71dff0d10ade40992252adfe2959aa8d7db
AntonVasilev <antonv901009@gmail.com>
2016-03-02 07:39:20 +0200

    PrintMyName

---
+++ PrintMyName/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class PrintMyName
+    {
+        static void Main()
+        {
+        Console.WriteLine("First Name");
+        Console.WriteLine("Last Name");
+        }
+    }
+
