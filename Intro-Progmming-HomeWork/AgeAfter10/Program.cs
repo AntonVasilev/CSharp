25932445e68f530f5c1c0189b31ea3f73bca9b5e
AntonVasilev <antonv901009@gmail.com>
2016-03-31 21:39:32 +0300

    New

---
+++ Intro-Progmming-HomeWork/AgeAfter10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AgeAfter10
+{
+    class Age
+    {
+        static void Main()
+        {
+            DateTime userBirthday = DateTime.Parse(Console.ReadLine());
+            DateTime timeToday = DateTime.Now;
+            long period = timeToday.Subtract(userBirthday).Ticks;
+            int years = new DateTime(period).Year - 1;
+            Console.WriteLine(years);
+            int futureYears = years + 10;
+            Console.WriteLine(futureYears);
+        }
+    }
+}
+
+        
