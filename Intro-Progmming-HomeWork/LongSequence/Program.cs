bfc30c766b934870207fa41db6dd52cd0983d8f8
AntonVasilev <antonv901009@gmail.com>
2016-03-31 21:40:25 +0300

    New

---
+++ Intro-Progmming-HomeWork/LongSequence/Program.cs
@@ -0,0 +1,36 @@
+﻿//Problem 16. Print Long Sequence
+//Description => Write a program that prints the first 1000 members of the sequence: 2, -3, 4, -5, 6, -7, …
+//Input => There is no input for this task.
+//Output => Output the first 1000 members of the sequence, each on a separate line.
+//2
+//-3
+//4
+//-5
+//6
+//...
+
+using System;
+namespace LongSequence
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.BufferHeight = 1001;
+            for (int i = 2; i < 1002; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i + "  ");
+
+                }
+                else
+                {
+                    Console.WriteLine(i * (-1) + "  " );
+                    
+             
+                }
+            }
+        }
+    }
+}
